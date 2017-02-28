f7d9da68b461eda722116c9e8793651a9d62957b
unknown <small.box@163.com>
2017-02-28 17:27:07 +0800

    1) ME909s821_ECM.sh bug fixed
    Signed-off-by: Heyn2016 <small.box@163.com>

--- Apalis iMX6/ME909s821_ECM.sh
+++ Apalis iMX6/ME909s821_ECM.sh
@@ -41,14 +41,16 @@
 
 webpath=/www/pages/htdocs/conf/AnyLink.xml
 ltemodulename="usb0"
+logfile=/tmp/lte.log
+
+echo "network-lte.service start" > $logfile
 
 #--------------------------------------------
 # User config end
 #--------------------------------------------
 
-
 if [ ! -f "$webpath" ]; then
-    echo $webpath file is not exist.
+    echo $webpath file is not exist. >> $logfile
     exit 0
 fi
 
@@ -67,8 +69,29 @@ do
         netmode=$item
         break
     fi
-done<$webpath
+done < $webpath
 
+# Debug
+netmode="4G"
+
+if [ "$netmode" == "gateway" ];then
+    echo Wired connection... >> $logfile
+    exit 0
+fi
+
+echo "Wireless connection" >> $logfile
+
+#---------------------------------------------------------------
+# Check PCIe LTE module
+#---------------------------------------------------------------
+# Bus 002 Device 003: ID 12d1:15c1 Huawei Technologies Co., Ltd.
+#---------------------------------------------------------------
+
+vidpid=$(lsusb | grep 'Huawei' | awk '{print $6}' | awk -F[':'] '{print $1 $2}')
+if [ "$vidpid" != "12d115c1" ];then
+    echo HUAWEI ME909s-821 Module not detected. >> $logfile
+    exit 0
+fi
 
 #--------------------------------------------
 # Check LTE module
@@ -77,27 +100,140 @@ done<$webpath
 for num  in 0 1 2 3 4
 do
     if [ ! -c "/dev/ttyUSB$num" ]; then
-        echo HUAWEI LTE Module is not exist.
+        echo HUAWEI LTE Module is not exist. >> $logfile
         exit 0
     fi
 done
 
+#--------------------------------------------
+# Start LED
+#--------------------------------------------
+echo -e "AT\r\n" > /dev/ttyUSB0
+echo -e "AT^LEDCTRL=1\r\n" > /dev/ttyUSB0
 
-# Debug
-#netmode="4G"
+#--------------------------------------------
+# Detect SIM Card <Method 1st>
+#--------------------------------------------
 
+# echo -e "AT^ICCID?\r\n" > /dev/ttyUSB0
+# while read lines
+# do
+#     if [[ "$lines" == *"ERROR"* ]];then
+#         echo SIM card not detected. $lines
+#         exit 0
+#     elif [[ "$lines" == *"^ICCID:"* ]];then
+#         echo $lines
+#         break;
+#     fi
+# done < /dev/ttyUSB0
 
-if [ "$netmode" == "4G" ];then
-    echo Start LTE connection...
-    # HUAWEI ME909s Series LTE Module
-    # Do not blink. (default value)
-    echo -e "AT^LEDCTRL=1\r\n" > /dev/ttyUSB0
-    echo -e "AT^NDISDUP=1,1\r\n" > /dev/ttyUSB0
-    udhcpc -i $ltemodulename
-else
-    echo Wired connection...
-    exit 0
-fi
+#--------------------------------------------
+# Detect SIM Card <Method 2nd>
+#--------------------------------------------
+
+# echo -e "AT^SYSINFOEX\r\n" > /dev/ttyUSB0
+# while read lines
+# do
+#     if [[ "$lines" == *"^SYSINFOEX:"* ]];then
+#         # <srv_status>: indicates the system service status.
+#         res=$(echo $lines | awk '{print $2}' | awk -F[','] '{print $1}')
+#         if [ "$res" == "2" ];then
+#             res=$(echo $lines | awk '{print $2}' | awk -F[','] '{print $4}')
+#             # <sim_state>: indicates the state of the SIM card.
+#             if [ "$res" == "1" ];then
+#                 break
+#             elif [ "$res" == "255" ];then
+#                 echo SIM card not detected. $lines
+#                 exit 0
+#             fi
+#         else
+#             echo No services or Restricted services or Restricted regional services. $lines
+#             exit 0
+#         fi 
+#     fi
+# done < /dev/ttyUSB0
+
+echo -e "AT^SYSINFOEX\r\n" > /dev/ttyUSB0
+while read lines
+do
+    if [[ "$lines" == *"^SYSINFOEX:"* ]];then
+        res=$(echo $lines | awk '{print $2}' | awk -F[','] '{print $4}')
+        # <sim_state>: indicates the state of the SIM card.
+        if [ "$res" == "1" ];then
+            break
+        elif [ "$res" == "255" ];then
+            echo SIM card not detected. $lines >> $logfile
+            exit 0
+        fi
+    else
+        n=$(($n + 1))
+        if [ "$n" -gt 100 ];then
+            echo SIM card not detected and timeout. $lines >> $logfile
+            exit 0
+        fi
+    fi
+done < /dev/ttyUSB0
+
+#--------------------------------------------
+# Query the Connection Status
+#--------------------------------------------
+
+# echo -e "AT\r\n" > /dev/ttyUSB0
+# echo -e "AT^NDISSTATQRY?\r\n" > /dev/ttyUSB0
+# while read lines
+# do
+#     if [[ "$lines" == *"^NDISSTATQRY:"* ]];then
+#         res=$(echo $lines | awk '{print $2}' | awk -F[','] '{print $1}')
+#         if [ "$res" == "1" ];then
+#             echo Close current network connection.
+#             echo -e "AT\r\n" > /dev/ttyUSB0
+#             echo -e "AT^NDISDUP=1,0\r\n" > /dev/ttyUSB0
+#             sleep 5s
+#             exit 0 
+#         else
+#             break
+#         fi
+#     fi
+# done < /dev/ttyUSB0
+
+#--------------------------------------------
+# Start LTE Internet mode
+#--------------------------------------------
+
+echo Start LTE connection...
+echo -e "AT\r\n" > /dev/ttyUSB0
+echo -e "AT^NDISDUP=1,1\r\n" > /dev/ttyUSB0
+
+while read lines
+do
+    if [[ "$lines" == *"ERROR"* ]];then
+        echo Start LTE Internet mode is $lines >> $logfile
+        exit 0
+    elif [[ "$lines" == *"OK"* ]];then
+        echo Start LTE Internet mode is $lines >> $logfile
+        break;
+    fi
+done < /dev/ttyUSB0
+
+#--------------------------------------------
+# Query the Connection Status
+#--------------------------------------------
+# sleep 3s
+# echo -e "AT\r\n" > /dev/ttyUSB0
+# echo -e "AT^NDISSTATQRY?\r\n" > /dev/ttyUSB0
+# while read lines
+# do
+#     if [[ "$lines" == *"^NDISSTATQRY:"* ]];then
+#         res=$(echo $lines | awk '{print $2}' | awk -F[','] '{print $1}')
+#         if [ "$res" == "1" ];then
+#             echo Connected Done
+#         else
+#             echo Disconnected Done
+#         fi
+#     fi
+# done < /dev/ttyUSB0
+
+udhcpc -i $ltemodulename
 
 
 address=$(ip addr show label $ltemodulename scope global | awk '$1 == "inet" { print $2,$4}')
@@ -119,10 +255,10 @@ gateway=$(route -n | grep 'UG[ \t]' | grep $ltemodulename | awk '{print $2}')
 dns=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
 
 
-echo ip:$ip,mask:$mask,broadcast:$broadcast,gateway:$gateway,dns:$dns
+echo ip:$ip,mask:$mask,broadcast:$broadcast,gateway:$gateway,dns:$dns >> $logfile
 
 if [ "$ip" == "" ];then
-    echo HUAWEI LTE Module dial failure.
+    echo HUAWEI LTE Module dial failure >> $logfile
     exit 1
 fi
 
@@ -134,7 +270,7 @@ do
     cloudaddr=`echo $line | awk -F['<'] '{print $1}'`
     
     if [ "$(route -n | grep $cloudaddr)" == "" ];then
-        echo Cloud IP Address = $cloudaddr
+        echo Cloud IP Address = $cloudaddr >> $logfile
         route add -net $cloudaddr netmask 255.255.255.255 dev $ltemodulename
     fi
 done
